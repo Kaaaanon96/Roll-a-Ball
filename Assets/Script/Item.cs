ba07ffac01482b3fe5db07385569fa3a26fdbdab
Kanon159 <c0114159d1@edu.teu.ac.jp>
2017-03-06 15:23:46 +0900

    first commit

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    // トリガーとの接触が起きた時に呼ばれるコールバック
+    void OnTriggerEnter (Collider hit) {
+
+        // 接触対象はPlayerタグですか？
+        if (hit.CompareTag ("Player")) {
+            // 何らかの処理
+            Destroy(gameObject);
+
+
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//ボールの移動用のスクリプト
+
+public class PlayerController : MonoBehaviour 
+{
+	// speedを制御する
+	public float speed = 25;
+
+	void FixedUpdate() {
+		// 入力をxとzに格納する
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		// 同一のgameObjectが持つrigidbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// rigidBodyの横(x)と奥(z)に力を加える
+        rigidbody.AddForce (x * speed, 0, z * speed);
+	}
+}
